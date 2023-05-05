765b378d3cc788730c8f251e55c8bd2b033f5638
celinacarnevale <102771516+celinacarnevale@users.noreply.github.com>
2023-05-05 19:52:46 -0300

    Comienzo de conexion con backend 05/05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -17,6 +17,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component'
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -38,9 +39,12 @@ import { LoginComponent } from './components/login/login.component'
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/socialmedia/socialmedia.component.ts
+++ src/app/components/socialmedia/socialmedia.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-socialmedia',
   templateUrl: './socialmedia.component.html',
   styleUrls: ['./socialmedia.component.css']
 })
-export class SocialmediaComponent {
-  constructor(private router:Router) { }
+
+export class SocialmediaComponent implements OnInit{
+
+  isLogged = false;
+
+  constructor(private router:Router, private tokenService: TokenService) { }
 
   ngOnInit(): void{
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
+
+  onLogOut(): void {
+    this.tokenService.logOut();
+    window.location.reload();
   }
 
   login(){
